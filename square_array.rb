4fede580fa32bb15162c6618056a5deb86e69fff
Irum F <irummf@gmail.com>
2018-08-25 19:09:32 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   squared_numbers = []
   array.each do |square_number|
-    squared_numbers.push(#{square_number}**2)
+    squared_numbers.push(square_number**2)
   squared_numbers
 end
 end
