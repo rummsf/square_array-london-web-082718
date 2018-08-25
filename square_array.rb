4e64cbe28e945196d1c434e1528d6370768de8e8
Irum F <irummf@gmail.com>
2018-08-25 18:36:08 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,13 +2,3 @@ def square_array(array)
   array.each do |square_number|
     puts Math.sqrt(#{square_number})
 end
-
-#def square_array(numbers)
-  numbers.each 
-#end
-
-#def square_array()
-  #numbers.each do
-  #puts Math.sqrt(numbers)
-#end
-#end 
